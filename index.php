90160ef1f859b6702d379de537c27d6776d65524
SebastianMacielAvila <jeffersonstarships90@gmail.com>
2021-03-02 20:29:34 -0600

    ADD: Agregar index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "Hola, mi nombre es Sebastián Maciel Ávila";
+echo "Tengo 21 años y mi cumpleaños es el 22 de septiembre";
+echo "Mis pasatiempos favoritos son: ";
+echo "- Jugar videojuegos";
+echo "- Hacer videos para mi canal en youtube";
+echo "- Hacer ejercicio y jugar diversos deportes";
+echo "Me gustaría trabajar en el campo de la programación,
+principalmente en el desarrollo de software o de videojuegos.
+Además, también me llama la atención el desarrollo web y el
+Big Data.";
+echo "GRACIAS POR SU ATENCIÓN";
+?>
